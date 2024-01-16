39c4c7c86d3f50794f4de3614157abe74c4b68da
colettebrochu <123673109+colettebrochu@users.noreply.github.com>
2024-01-16 20:28:57 +0000

    countdown

--- sketch.js
+++ sketch.js
@@ -1,22 +1,10 @@
-let square = rect(300, 300, 50, 50);
-let circle = ellipse(400, 400, 50, 50);
-let value = rect(mouseX, mouseY, 1, 1);
-function setup () {
+//let s = 300 - second();
+
+function setup (){
   createCanvas(500,500);
 }
 
 function draw () {
   background(150);
-
-  ellipse(200, 200, 50, 50);
-  ellipse(mouseX, mouseY, 50, 50);
-  if (mouseIsPressed === true) {
-    rect(mouseX-25, mouseY-25, 50, 50);
-  }
-}
-
-function mouseClicked() {
-  if(value === square){
-    rect(mouseX, mouseY, 50, 50);
-  }
+  text("time left in game" + (300-second()), 5, 50);
 }
