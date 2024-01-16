ff265eb12d268de0798ad263a8170d2af087ea48
colettebrochu <123673109+colettebrochu@users.noreply.github.com>
2024-01-16 21:42:07 +0000

    sort of timer

--- sketch.js
+++ sketch.js
@@ -1,10 +1,28 @@
-//let s = 300 - second();
+let a = 330
 
 function setup (){
   createCanvas(500,500);
 }
 
+const timer = setInterval(() => {
+  a--;
+}, 1000);
+
 function draw () {
   background(150);
-  text("time left in game" + (300-second()), 5, 50);
+  fill(255);
+  rect(415, 0, 100, 50);
+  fill(0);
+  textSize(20);
+  text('Time left', 420, 20)
+  text(a, 445, 40);
+    if (a <= 0) {
+      background(0);
+      textSize(75);
+      fill('red');
+      text('Game Over!', width / 2, height / 2);
+    }
 }
+
+
+  
