c01f26ea489b9bf6e5f0f56a7d0987509c60b101
colettebrochu <123673109+colettebrochu@users.noreply.github.com>
2024-01-16 19:48:56 +0000

    stuff

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+let square = rect(300, 300, 50, 50);
+let circle = ellipse(400, 400, 50, 50);
+let value = rect(mouseX, mouseY, 1, 1);
 function setup () {
   createCanvas(500,500);
 }
 
 function draw () {
   background(150);
+
+  ellipse(200, 200, 50, 50);
+  ellipse(mouseX, mouseY, 50, 50);
+  if (mouseIsPressed === true) {
+    rect(mouseX-25, mouseY-25, 50, 50);
+  }
+}
+
+function mouseClicked() {
+  if(value === square){
+    rect(mouseX, mouseY, 50, 50);
+  }
 }
